279ae1bb103f199251b8317c96f8af9a37b2da8a
Alexander Leonov <spawnleon@gmail.com>
2022-04-09 19:08:16 +0300

    init: start project portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,157 @@
+import i18n from './translate.js';
+
+
+const THEME_NAME_LIGHT = 'light';
+const THEME_NAME_DARK = 'dark';
+const LANG_BUTTONS = document.querySelectorAll('[data-js-lang]');
+
+
+const openMenu = () => {
+  const burgerMenuNav = document.querySelector('[data-js="burger-menu-nav"]');
+  if (burgerMenuNav) {
+    burgerMenuNav.classList.add('open');
+  }
+};
+
+const closeMenu = () => {
+  const burgerMenuNav = document.querySelector('[data-js="burger-menu-nav"]');
+  if (burgerMenuNav) {
+    burgerMenuNav.classList.remove('open');
+  }
+};
+
+const changeImage = (season) => {
+  const images = document.querySelectorAll('[data-js="portfolio-items"] img');
+  images.forEach((img, index) => img.src = `./assets/content/${season}/${index + 1}.jpg`);
+};
+
+const preloadImages = () => {
+  const seasons = [
+    'winter', 'spring', 'summer', 'autumn'
+  ];
+
+  seasons.forEach(season => {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/content/${season}/${i}.jpg`;
+    }
+  });
+
+};
+
+const translate = (lang = 'en') => {
+  document.querySelectorAll('[data-i18]').forEach(currentElement => {
+
+    const i18nKey = currentElement.dataset['i18'];
+    if (currentElement.placeholder) {
+      currentElement.placeholder = i18n[lang][i18nKey];
+    } else {
+      currentElement.textContent = i18n[lang][i18nKey];
+    }
+
+    window.localStorage.setItem('lang', lang);
+  });
+};
+
+const setTheme = (theme = THEME_NAME_LIGHT) => {
+  theme === THEME_NAME_LIGHT
+    ? document.body.classList.add(THEME_NAME_LIGHT)
+    : document.body.classList.remove(THEME_NAME_LIGHT);
+  localStorage.setItem('theme', theme);
+};
+
+const setLang = (lang = 'en') => {
+  LANG_BUTTONS.forEach(btn => {
+    btn.classList.remove('lang__link--active');
+  });
+  const activeButton = document.querySelector(`[data-js-lang="${lang}"]`);
+  if (activeButton) {
+    activeButton.classList.add('lang__link--active');
+  }
+
+  translate(lang);
+
+};
+
+const main = () => {
+  const lang = window.localStorage.getItem('lang');
+  setLang(lang);
+
+  const burgerBtn = document.querySelector('[data-js="burger-button"]');
+
+  if (burgerBtn) {
+    burgerBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      //TODO: decomposition this to toggleMenu
+      const isActive = burgerBtn.dataset['isActive'] === 'true';
+      burgerBtn.dataset['isActive'] = !isActive;
+      burgerBtn.classList.toggle('burger--active');
+      isActive ? closeMenu() : openMenu();
+
+    });
+  }
+
+  const mainMenu = document.querySelector('[data-js="main-menu"]');
+  if (mainMenu) {
+    mainMenu.addEventListener('click', (evt) => {
+      if (evt.target.dataset.js === 'main-menu-link') {
+
+        //TODO: decomposition this to toggleMenu
+        const isActive = burgerBtn.dataset['isActive'] === 'true';
+        burgerBtn.dataset['isActive'] = !isActive;
+        burgerBtn.classList.toggle('burger--active');
+        isActive ? closeMenu() : openMenu();
+      }
+    });
+  }
+
+  const portfolioButtons = document.querySelectorAll('[data-season]');
+  if (portfolioButtons) {
+
+    portfolioButtons.forEach(btn => {
+      btn.addEventListener('click', (evt => {
+
+        portfolioButtons.forEach(btn => {
+          btn.classList.add('button--transparent');
+        });
+
+        evt.target.classList.remove('button--transparent');
+
+        const season = evt.target.dataset.season;
+        changeImage(season);
+
+      }));
+    });
+  }
+
+  if (LANG_BUTTONS) {
+    LANG_BUTTONS.forEach(btn => {
+      btn.addEventListener('click', (evt) => {
+        const { jsLang: lang } = evt.target.dataset;
+        setLang(lang);
+      });
+
+    });
+  }
+
+  const themeButton = document.querySelector('[data-js="theme-button"]');
+  if (themeButton) {
+    themeButton.addEventListener('click', (evt) => {
+      const theme = localStorage.getItem('theme') || THEME_NAME_LIGHT;
+      const nextTheme = theme === THEME_NAME_LIGHT
+        ? THEME_NAME_DARK
+        : THEME_NAME_LIGHT;
+      setTheme(nextTheme);
+    });
+
+  }
+
+  const theme = localStorage.getItem('theme') || THEME_NAME_LIGHT;
+
+  setTheme(theme);
+  preloadImages();
+
+};
+
+main();
---
+++ assets/js/video.js
@@ -0,0 +1,111 @@
+const inputEvt = new Event('input');
+
+const viewer = document.querySelector('.viewer');
+const playBtn = document.querySelector('[data-js="play-button"]');
+const controlPlayBtn = document.querySelector('[data-js="control-play-button"]');
+const volumeBtn = document.querySelector('[data-js="volume-button"]');
+const fullscreenBtn = document.querySelector('[data-js="fullscreen-button"]');
+const progressRange = document.querySelector('[data-js="progress-range"]');
+const volumeRange = document.querySelector('[data-js="volume-range"]');
+
+let currentVolume = 0.5;
+let progression;
+
+const togglePlay = (evt) => {
+  evt.preventDefault();
+  if (viewer.paused) {
+    viewer.play();
+    controlPlayBtn.textContent = '❚❚';
+    progression = window.setInterval(updateProgress, 200);
+  } else {
+    viewer.pause();
+    controlPlayBtn.textContent = '►';
+    clearInterval(progression);
+  }
+};
+
+const toggleVolume = (evt) => {
+  evt.preventDefault();
+
+  if (viewer.muted) {
+    viewer.muted = false;
+    volumeBtn.classList.remove('muted');
+    volumeRange.value = currentVolume;
+
+  } else {
+    viewer.muted = true;
+    volumeBtn.classList.add('muted');
+    volumeRange.value = 0;
+  }
+  volumeRange.dispatchEvent(inputEvt);
+};
+
+const playHandler = (evt) => {
+  evt.preventDefault();
+  if (viewer.paused) {
+    playBtn.style.display = 'block';
+  } else {
+    playBtn.style.display = 'none';
+  }
+};
+
+const setRangeBackground = (element, value) => {
+  element.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value *
+  100}%, #c4c4c4 ${value * 100}%, #c4c4c4 100%)`;
+};
+
+const progressRangeHandler = (evt) => {
+  const element = evt.target;
+  const value = element.value;
+  if (viewer.duration) {
+    viewer.currentTime = value * viewer.duration;
+  }
+  setRangeBackground(element, value);
+};
+
+const volumeRangeHandler = (evt) => {
+  const { value } = volumeRange;
+  viewer.volume = volumeRange.value;
+  if (value === '0') {
+    volumeBtn.classList.add('muted');
+  } else {
+    volumeBtn.classList.remove('muted');
+  }
+
+  setRangeBackground(volumeRange, value);
+};
+
+const fullscreenHandler = () => {
+  viewer.requestFullscreen().then();
+};
+
+const updateProgress = () => {
+  const progress = viewer.currentTime / viewer.duration;
+  progressRange.value =  progress;
+  setRangeBackground(progressRange, progress);
+};
+
+playBtn.addEventListener('click', togglePlay);
+controlPlayBtn.addEventListener('click', togglePlay);
+viewer.addEventListener('click', togglePlay);
+viewer.addEventListener('play', playHandler);
+
+viewer.addEventListener('pause', playHandler);
+volumeBtn.addEventListener('click', toggleVolume);
+fullscreenBtn.addEventListener('click', fullscreenHandler);
+progressRange.addEventListener('input', progressRangeHandler);
+volumeRange.addEventListener('input', volumeRangeHandler);
+viewer.addEventListener('ended', (evt) => {
+
+  viewer.currentTime = 0;
+  progressRange.value = 0;
+  progressRange.dispatchEvent(inputEvt);
+  playHandler(evt);
+  controlPlayBtn.textContent = '►';
+
+});
+
+//
+
+progressRange.dispatchEvent(inputEvt);
+volumeRange.dispatchEvent(inputEvt);
